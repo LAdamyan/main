754616cd9112c2b7a2cfb25e3c3c98f9344f9273
LAdamyan <ladamyan@bk.ru>
2021-10-22 02:26:41 +0400

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.third_activity);
+        setContentView(R.layout.change_password);
+
+        AppCompatTextView newPassword = findViewById(R.id.new_password);
+        newPassword.setText("New password");
+        newPassword.setTextColor(ContextCompat.getColor(this,R.color.black));
+
+        AppCompatButton saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(view -> Toast.makeText(MainActivity.this,"Your password has changed ",
+                Toast.LENGTH_LONG).show());
+
+
+
+
     }
 }
