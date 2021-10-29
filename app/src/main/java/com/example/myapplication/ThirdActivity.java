d6a1ab871bc4a4aa93e68f02d4432dc77bf8ccfd
LAdamyan <ladamyan@bk.ru>
2021-10-29 22:46:08 +0400

    homework8

--- app/src/main/java/com/example/myapplication/ThirdActivity.java
+++ app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -1,15 +1,35 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ThirdActivity extends AppCompatActivity {
 
+    TextView textView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_activity);
+        setContentView(R.layout.third_activity2);
+
+        setSpinnerAdapter();
     }
+
+    private void setSpinnerAdapter() {
+        Spinner spinner = findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.colors_array,
+                android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+    }
+
+
+
 }
+
