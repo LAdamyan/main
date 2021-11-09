255ad800422a8d07f9d840031d2f9edd7f9bd685
LAdamyan <ladamyan@bk.ru>
2021-11-09 17:46:52 +0400

    homework12

--- app/src/main/java/com/example/myapplication/TestActivity.java
+++ app/src/main/java/com/example/myapplication/TestActivity.java
@@ -10,6 +10,6 @@ public class TestActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.scroll_layout);
+        setContentView(R.layout.chip_layout);
     }
 }
