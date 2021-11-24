9baf90ce3eb4d02b0a2c7ec871dc7509d3820861
LAdamyan <ladamyan@bk.ru>
2021-11-24 18:53:03 +0400

    homework18

--- app/src/main/java/com/example/myapplication/fragment/FullImageFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/FullImageFragment.java
@@ -3,20 +3,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.example.myapplication.Gallery;
 import com.example.myapplication.R;
+
+import java.util.ArrayList;
 
 
 public class FullImageFragment extends Fragment {
+
+    AppCompatImageView imageView;
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view2 = inflater.inflate(R.layout.fragment_full_image, container, false);
-       return  view2;
+        View view =   inflater.inflate(R.layout.fragment_full_image, container, false);
+        imageView = view.findViewById(R.id.full_image);
+        loadFullImage();
+        return  view;
+
     }
 
+    void loadFullImage() {
 
+        if (getActivity() != null) {
+            Glide.with(getActivity())
+                    .load(Gallery.getGlideImages())
+                    .placeholder(R.drawable.loading)
+                    .error(R.drawable.error)
+                    .centerCrop()
+                    .into(imageView);
+
+        }
+
+    }
 }
